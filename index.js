d42cbe481b6c3e0c8dbb173bfb019216e46938d1
Kantesh Prasad H K <hk.kanteshprasad@gmail.com>
2023-08-21 14:53:07 +0530

    First Commit
    My portfolio Page

---
+++ index.js
@@ -0,0 +1,21 @@
+AOS.init();
+
+window.onload = function() {
+    // Hide the loading spinner once the page is fully loaded
+    document.querySelector('.spinner-overlay').style.display = 'none';
+    
+   
+  };
+
+  var typed = new Typed('#element', {
+    strings: ['Web Designing','Web Development','Video Editing','Content Writing','Search Engine','Analytics '],
+    typeSpeed: 80,
+    startDelay: 2600,
+    showCursor: true,
+    loop: true,
+    
+  });
+
+
+
+ 
